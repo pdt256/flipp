0defe8bf0f373921696a093129b83a3082d996ad
Jeremy Collins <jdodsoncollins@gmail.com>
2016-09-14 20:05:13 -0700

    basis js

--- app.js
+++ app.js
@@ -2,21 +2,49 @@ $(document).ready(function(){
 
   flipp.init();
 
+  $('.vote-icon').on('click', function(event) {
+        console.log('clicked item id', event.target.id);
+        flipp.castVote(event.target.id);
+    });
+
 });
 
+// jquery module pattern, call flipp.methodName() above via event listeners
 var flipp = {
-  
+
   init: function(){
+
+    // initialize: init firebase and carousel
+    var config = {
+      apiKey: "<API_KEY>",
+      authDomain: "<PROJECT_ID>.firebaseapp.com",
+      databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+      storageBucket: "<BUCKET>.appspot.com",
+    };
+    firebase.initializeApp(config);
+
     $('.owl-carousel').owlCarousel({
       loop: true
     });
   },
 
+  firebase: {},
+
   castVote: function(item) {
-    $.post( "ajax/test.html", function( data ) {
-      console.log(data)
-    }, function(err) {
-      console.log(err);
-    });
+    var request = $.ajax({
+				data: item,
+				method: 'PUT',
+				url: 'SOMEURL',
+			});
+
+		request.then(function (res) {
+			if (res && res.result) {
+				// success
+
+			} else {
+				console.log('something went wrong', res);
+			}
+		});
   }
+
 }
